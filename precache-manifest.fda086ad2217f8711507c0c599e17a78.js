b9c0c37a72e753bb6fc179dbb0ddb1d986d84d55
neilkinpark <neilkinpark@gmail.com>
2023-02-25 20:05:38 +0800

    Updates

---
+++ precache-manifest.fda086ad2217f8711507c0c599e17a78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c6316ce0cde92a03009",
+    "url": "/allotment/static/css/main.16548198.chunk.css"
+  },
+  {
+    "revision": "8c6316ce0cde92a03009",
+    "url": "/allotment/static/js/main.8c6316ce.chunk.js"
+  },
+  {
+    "revision": "483a0879fa0e6e687539",
+    "url": "/allotment/static/js/1.483a0879.chunk.js"
+  },
+  {
+    "revision": "d3ea715868a4b554a125",
+    "url": "/allotment/static/js/2.d3ea7158.chunk.js"
+  },
+  {
+    "revision": "8e4618dd11e510910b77",
+    "url": "/allotment/static/js/runtime~main.8e4618dd.js"
+  },
+  {
+    "revision": "789fbfb065609e1fb9fd9a06dfb7d602",
+    "url": "/allotment/index.html"
+  }
+];
